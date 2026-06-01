a93c2ecdf8266765800451a44aeff593a8805494
DESKTOP-V6Q5LBH\User <vincentleroy.web@gmail.com>
2026-06-01 13:25:18 +0200

    add index.html and slide01 example

---
+++ slide01/layouts/center-bg.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  background?: string
+}>()
+
+const style = computed(() => ({
+  backgroundImage: props.background ? `url('${props.background}')` : undefined,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  width: '100%',
+  height: '100%',
+}))
+</script>
+
+<template>
+  <div class="slidev-layout center h-full grid place-content-center" :style="style">
+    <slot />
+  </div>
+</template>
+
+<style>
+.slidev-layout * {
+  -webkit-text-size-adjust: none;
+  text-size-adjust: none;
+}
+.slidev-layout h1 { font-size: xxx-large; line-height: 1.1; font-weight: 800; }
+.slidev-layout h2 { font-size: xx-large; line-height: 1.2; font-weight: 700; }
+.slidev-layout h3 { font-size: x-large; line-height: 1.3; font-weight: 600; margin: auto 2.5%; text-align: center;}
+.slidev-layout h4 { font-size: large; line-height: 1.4; font-weight: 500; margin: auto 2.5% }
+.slidev-layout h5 { font-size: medium; line-height: 1.5; font-weight: 400; margin: auto 2.5% }
+.slidev-layout h6 { font-size: small; line-height: 1.6; font-weight: 300; margin: auto 2.5% }
+.slidev-layout p  { font-size: 0.9em; line-height: 1.7; margin: auto 4%; }
+.slidev-layout ul          { list-style-type: disc; padding-left: 1.5rem; margin: auto 4% ; font-size: 0.9em; line-height: 1.7;}
+.slidev-layout ol          { list-style-type: decimal; padding-left: 1.5rem; margin: auto 4%; font-size: 0.9em; line-height: 1.7;}
+.slidev-layout ul ul       { list-style-type: circle; font-size: 0.8em;}
+.slidev-layout ul ul ul    { list-style-type: square; font-size: 0.7em;}
+.slidev-layout li          { margin: 0.25rem 0; }
+.slidev-layout table       { border-collapse: collapse; width: 100%; }
+.slidev-layout td          { border: 1px solid rgba(255,255,255,0.5); padding: 0.5rem 1rem; background: rgba(255,255,255,0.166); font-size: 0.75rem; }
+.slidev-layout th          { border: 1px solid rgba(255,255,255,0.5); padding: 0.5rem 1rem; background: rgba(255,255,255,0.33); font-weight: 600; font-size: 0.85rem; }
+</style>
---
+++ slide01/slides.md
@@ -0,0 +1,56 @@
+---
+theme: seriph
+title: Slidev Slide01
+# canvasWidth: 1280
+aspectRatio: 16/9
+defaults:
+    layout: center-bg
+    background: /background-1.png
+    class: text-white
+routerMode: hash
+fit: false
+---
+
+# Slide01
+___
+
+
+---
+
+## Examples
+___
+
+
+---
+
+### A simple code example
+___
+
+<br />
+
+Voici un exemple en batch :
+
+
+```bat {0|2-3|4|5|*}
+@echo off
+SET /A number_a = 5
+SET /A number_b = 3
+SET /A result = %number_a% + %number_b%
+echo %result%
+```
+
+
+
+
+---
+
+### A simple HTML example
+
+<br />
+
+<div style="display: flex; justify-content: center; ">
+
+ <img src="/img01.png" width="300" />
+
+</div>
+
